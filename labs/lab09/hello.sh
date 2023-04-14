10a153553c54fbff46cbfa86f64a28edf9c11302
nikitavlaskin <1132210696@pfur.ru>
2023-04-14 10:58:04 +0300

    lab 10

---
+++ labs/lab09/hello.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+HELLO=Hello
+function hello
+{
+local HELLO=World
+echo $HELLO
+}
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+hello
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+helloecho $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+echo $HELLO
+hello
